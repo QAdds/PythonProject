4d0957d6ae5e4d63758171d4afd33f553ab6d95b
denis <Klimenkodenis439@gmail.com>
2026-05-14 12:07:58 +0300

    roles.py

--- Modul 4/CocountQA_Hardsound1/Cinescope/api/user_api.py
+++ Modul 4/CocountQA_Hardsound1/Cinescope/api/user_api.py
@@ -1,7 +1,5 @@
-from constants import BASE_URL, USER_ENDPOINT
+from constants import USER_BASE_URL, USER_ENDPOINT
 from custom_requester.custom_requester import CustomRequester
-
-USER_ENDPOINT = "/user"
 
 
 class UserAPI(CustomRequester):
@@ -10,17 +8,30 @@ class UserAPI(CustomRequester):
     """
 
     def __init__(self, session):
-        super().__init__(session=session, base_url=BASE_URL)
+        super().__init__(session=session, base_url=USER_BASE_URL)
 
-    def get_user_info(self, user_id, expected_status=200):
+    def get_user(self, user_locator, expected_status=200):
         """
-        Получение информации о пользователе.
-        :param user_id: ID пользователя.
+        Получение пользователя по id или email.
+        :param user_locator: ID пользователя или email.
         :param expected_status: Ожидаемый статус-код.
         """
         return self.send_request(
             method="GET",
-            endpoint=f"{USER_ENDPOINT}/{user_id}",
+            endpoint=f"{USER_ENDPOINT}/{user_locator}",
+            expected_status=expected_status
+        )
+
+    def create_user(self, user_data, expected_status=201):
+        """
+        Создание пользователя.
+        :param user_data: Данные нового пользователя.
+        :param expected_status: Ожидаемый статус-код.
+        """
+        return self.send_request(
+            method="POST",
+            endpoint=USER_ENDPOINT,
+            data=user_data,
             expected_status=expected_status
         )
 
